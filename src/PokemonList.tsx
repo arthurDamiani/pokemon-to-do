50116278b24547c550535bfeb08d8d97ae2dc915
arthurDamiani <arthur.damiani@gmail.com>
2024-01-12 12:09:00 -0300

    remove unecessary import of react

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { useQuery } from 'react-query';
 import { getAllPokemons, getPokemons } from './services/pokemon.service';
 import { PokemonDetailedItem, SimplePokemonItem } from './interfaces/pokemon';
--- src/components/PokemonBox.tsx
+++ src/components/PokemonBox.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { PokemonDetailedItem } from '../interfaces/pokemon';
 import { PokemonListContext } from '../contexts/PokemonList.context';
 
